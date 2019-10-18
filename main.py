a3fadfd6f3183f03a428cbb082fab2bd5f52a71e
UlyanitskayaD <ulyanitskdar@gmail.com>
2019-10-18 10:31:07 +0700

    commit1

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 #draw
+import turtle
+class Square:
+    # to do Ira
+    def __init__(self, x, y, size, color,rectangle):
+        pass
+    def show(self):
+        pass
 
+class Triangle:
+    # to do Milana
+    def __init__(self, x, y, size, color,rectangle):
+        pass
+    def show(self):
+        pass
+
+
+class Diamond:
+    # to do Alisa
+    def __init__(self, x, y, size, color,rectangle):
+        pass
+    def show(self):
+        pass
