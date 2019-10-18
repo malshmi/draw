750100607d12452e7de32f0088309677d14dc7c4
panterrrushkal <panterrrushka@gmail.com>
2019-10-18 11:01:02 +0700

    the First Commit

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 import turtle
 class Square:
     # to do Ira
-    def __init__(self, x, y, size, color,rectangle):
-        pass
+    def __init__(self, x, y, size, color):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.color = color
+
     def show(self):
-        pass
+        turtle.up()
+        turtle.goto(self.x, self.y)
+        turtle.down()
+        for _ in range (4):
+            turtle.forward(self.size)
+            turtle.left(90)
 
 class Triangle:
     # to do Milana
