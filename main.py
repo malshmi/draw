83bf88ba4bd2be8363ea3c6b640bd6ffb2405217
UlyanitskayaD <ulyanitskdar@gmail.com>
2019-10-25 09:03:52 +0700

    #HOUSE

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #draw
-from turtle import *
+
+import turtle as tl
+
 class Square:
     # to do Ira
     def __init__(self, x, y, size, color):
@@ -9,14 +11,16 @@ class Square:
         self.color = color
 
     def show(self):
-        turtle.up()
-        turtle.goto(self.x, self.y)
-        turtle.color(self.color)
-        turtle.down()
+        tl.up()
+        tl.goto(self.x, self.y)
+        tl.color(self.color)
+        tl.down()
+        tl.fillcolor(self.color)
+        tl.begin_fill()
         for _ in range (4):
-            turtle.forward(self.size)
-            turtle.left(90)
-        turtle.begin_fill()
+            tl.forward(self.size)
+            tl.left(90)
+        tl.end_fill()
 
 class Triangle:
     # to do Milana
@@ -27,14 +31,16 @@ class Triangle:
         self.color = color
         
     def show(self):
-        turtle.up()
-        turtle.goto(self.x, self.y)
-        turtle.color(self.color)
-        turtle.down()
+        tl.up()
+        tl.goto(self.x, self.y)
+        tl.color(self.color)
+        tl.down()
+        tl.fillcolor(self.color)
+        tl.begin_fill()
         for _ in range (3):
-            turtle.forward(self.size)
-            turtle.left(60)
-        turtle.begin_fill()
+            tl.forward(self.size)
+            tl.left(120)
+        tl.end_fill()
 
 class Parallelogram:
     def __init__(self, x, y, size, length, color, angle):
@@ -45,14 +51,50 @@ class Parallelogram:
         self.angle = angle
         self.length = length
     def show(self):
-        turtle.up()
-        turtle.goto(self.x, self.y)
-        turtle.color(self.color)
-        turtle.down()
+        tl.up()
+        tl.goto(self.x, self.y)
+        tl.color(self.color)
+        tl.down()
+        tl.fillcolor(self.color)
+        tl.begin_fill()
         for _ in range (2):
-            turtle.forward(self.size)
-            turtle.left(self.angle)
-            turtle.forward(self.length)
-            turtle.left(180-self.angle)
-        turtle.begin_fill()
-            
+            tl.forward(self.size)
+            tl.left(self.angle)
+            tl.forward(self.length)
+            tl.left(180-self.angle)
+        tl.end_fill()
+
+class Circle:
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+    def show(self):
+        tl.up()
+        tl.goto(self.x, self.y)
+        tl.color(self.color)
+        tl.down()
+        tl.fillcolor(self.color)
+        tl.begin_fill()
+        tl.circle(self.radius)
+        tl.end_fill()
+
+
+#f1 = Parallelogram( 0, 0, 100, 110, "green", 45)
+#f1.show()
+
+#f2 = Triangle(0, 60, 60, "red")
+#f2.show()
+
+f3= Square( 0, 0, 150, "pink")
+f3.show()
+
+f2 = Triangle(-10, 150, 170, "lightblue")
+f2.show()
+
+f5 = Circle(75,45,30, "lightgreen")
+f5.show()
+
+tl.mainloop()
+
